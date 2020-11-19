44161849d3eb972210b89c7a76039ecebb247054
Christian Tschenett <developer@furthermore.ch>
2020-11-19 15:59:01 +0100

    cleanups

--- test.sh
+++ test.sh
@@ -2,8 +2,7 @@
 
 export AWS_PROFILE="kovit"
 sls invoke --stage dev -f hello --data '{"message":"hello world", "recursive":true}'
-#sls invoke --stage dev -f hello --data '{"message":"error"}'
 
 unset AWS_PROFILE
 sls invoke --stage dev2 -f hello --data '{"message":"hello world", "recursive":true}'
-#sls invoke --stage dev2 -f hello --data '{"message":"error", "recursive":true}'
+sls invoke --stage dev2 -f hello --data '{"message":"error", "recursive":true}'
